568314e2b399940224d5e4ff4f370aad3b1a6ee9
fredkk22 <freddykwak123@gmail.com>
2022-08-10 09:45:03 -0400

    Added Develop folder

---
+++ Develop/index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What will be the title of your README?',
+        name: 'title'
+    },
+    {
+
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
